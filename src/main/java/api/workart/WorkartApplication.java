0868bce29661b7626ab9c7806ca96ce6a03ce5d8
Natalia <natikbahamon@gmail.com>
2020-07-10 16:05:49 -0500

    first commit

---
+++ src/main/java/api/workart/WorkartApplication.java
@@ -0,0 +1,13 @@
+package api.workart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/workart/WorkartApplicationTests.java
@@ -0,0 +1,13 @@
+package api.workart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
